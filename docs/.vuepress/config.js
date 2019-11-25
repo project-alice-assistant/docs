76595738d3717d9074659fcb24d813b46a27538b
maxbachmann <ssh@maxbachmann.de>
2019-11-25 01:02:12 +0100

    add some metadata

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,13 @@ module.exports = {
 	lang: 'en-US',
 	title: 'ProjectAlice',
 	description: 'A frontend library to develop zoomable user interfaces',
+	head: [
+		['link', { rel: 'apple-touch-icon', sizes: '180x180', href: `/icons/apple-touch-icon.png` }],
+		['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: `/icons/favicon-32x32.png` }],
+		['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: `/icons/favicon-16x16.png` }],
+		['meta', { name: 'apple-mobile-web-app-title', content: 'ProjectAlice' }],
+		['meta', { name: 'application-name', content: 'ProjectAlice Docs' }],
+	  ],
 	serviceWorker: true,
 	themeConfig: {
 	  repo: 'project-alice-powered-by-snips/ProjectAlice',
