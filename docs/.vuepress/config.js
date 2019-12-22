bc6fe4e8adfa2c214ca5916a9b65f489cfe8b73a
maxbachmann <kontakt@maxbachmann.de>
2019-12-22 08:11:19 +0100

    add nav topics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,55 +1,94 @@
 module.exports = {
-	base: '/docs/',
-	lang: 'en-US',
-	title: 'ProjectAlice',
-	description: 'A smart voice assistant that guarantees your privacy, powered by Snips',
-	head: [
-		['link', { rel: 'apple-touch-icon', sizes: '180x180', href: `/icons/apple-touch-icon.png` }],
-		['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: `/icons/favicon-32x32.png` }],
-		['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: `/icons/favicon-16x16.png` }],
-		['meta', { name: 'apple-mobile-web-app-title', content: 'ProjectAlice' }],
-		['meta', { name: 'application-name', content: 'ProjectAlice Docs' }],
-	  ],
-	serviceWorker: true,
-	themeConfig: {
-	  repo: 'project-alice-powered-by-snips/ProjectAlice',
-	  docsRepo: 'project-alice-powered-by-snips/docs',
-	  docsDir: 'docs',
-	  editLinks: true,
-	  sidebarDepth: 2,
-	  nav: [
-		{
-		  text: 'Contributing',
-		  link: '/contribute/'
-		},
-		{
-			text: 'Skill Development',
-			link: '/skill-development/'
-		  }
-	  ],
-	  sidebar: {
-		'/contribute/': [
-			{
-			  title: 'Contribute',
-			  collapsable: false,
-			  children: [
-				'',
-				'code-guidelines'
-			  ]
-			}
-		  ],
-		'/skill-development/': [
-			{
-			  title: 'Skill Development',
-			  collapsable: false,
-			  children: [
-				'',
-				'your-first-skill',
-				'logging',
-				'events'
-			  ]
-			}
-		  ],
-		}
-	}
+  base: '/docs/',
+  lang: 'en-US',
+  title: 'ProjectAlice',
+  description: 'A smart voice assistant that guarantees your privacy, powered by Snips',
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: `/icons/apple-touch-icon.png` }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: `/icons/favicon-32x32.png` }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: `/icons/favicon-16x16.png` }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'ProjectAlice' }],
+    ['meta', { name: 'application-name', content: 'ProjectAlice Docs' }],
+    ],
+  serviceWorker: true,
+  themeConfig: {
+    repo: 'project-alice-assistant/ProjectAlice',
+    docsRepo: 'project-alice-assistant/docs',
+    docsDir: 'docs',
+    editLinks: true,
+    sidebarDepth: 2,
+    nav: [
+      {
+        text: 'Set Up',
+        link: '/set-up/'
+      },
+      {
+        text: 'Using Alice',
+        link: '/usage/'
+      },
+      {
+        text: 'Contributing',
+        link: '/contribute/'
+      },
+      {
+        text: 'Skill Development',
+        link: '/skill-development/'
+      },
+      {
+        text: 'Core Development',
+        link: '/core-development/'
+      }
+    ],
+    sidebar: {
+      '/set-up/': [
+        {
+          title: 'Set Up',
+          collapsable: false,
+          children: [
+            ''
+          ]
+        }
+      ],
+      '/usage/': [
+        {
+          title: 'Using Alice',
+          collapsable: false,
+          children: [
+            ''
+          ]
+        }
+      ],
+      '/contribute/': [
+        {
+          title: 'Contribute',
+          collapsable: false,
+          children: [
+            '',
+            'code-guidelines'
+          ]
+        }
+      ],
+      '/skill-development/': [
+        {
+          title: 'Skill Development',
+          collapsable: false,
+          children: [
+            '',
+            'your-first-skill',
+            'logging',
+            'events'
+          ]
+        }
+      ],
+      '/core-development/': [
+        {
+          title: 'Core Development',
+          collapsable: false,
+          children: [
+            ''
+          ]
+        }
+      ]
+    }
   }
+}
