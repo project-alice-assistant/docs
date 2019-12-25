df3792c418539c28b35c36d595f2581fbddd10a6
maxbachmann <kontakt@maxbachmann.de>
2019-12-25 11:23:29 +0100

    allow loading self for prefetching

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   title: 'ProjectAlice',
   description: 'A smart voice assistant that guarantees your privacy',
   head: [
-    ['meta', { 'http-equiv': 'Content-Security-Policy', 'content': "default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'" }],
+    ['meta', { 'http-equiv': 'Content-Security-Policy', 'content': "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'" }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: `/icons/apple-touch-icon.png` }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: `/icons/favicon-32x32.png` }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: `/icons/favicon-16x16.png` }],
