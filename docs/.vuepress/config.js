ff1e4a8ff55e2d5a50f1e668bd906af93b075bfb
maxbachmann <kontakt@maxbachmann.de>
2019-12-25 11:49:06 +0100

    fix list syntax

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   description: 'A smart voice assistant that guarantees your privacy',
   head: [
     ['meta', { 'http-equiv': 'Content-Security-Policy', 'content': "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; frame-ancestors 'none'" }],
-    ['meta', { name="referrer", content="no-referrer"}]
+    ['meta', { name='referrer', content='no-referrer'}],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: `/icons/apple-touch-icon.png` }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: `/icons/favicon-32x32.png` }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: `/icons/favicon-16x16.png` }],
