034990520a644952603d53a49e385ae6e2e680a9
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-10-02 12:49:12 +0200

    :art: maskable icon and viewport

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,6 +54,7 @@ module.exports = {
     ['meta', { name: 'msapplication-TileImage', content: '/icons/favicon-64x64.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000' }],
     ['meta', { name: 'theme-color', content: '#000' }]
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }]
   ],
   serviceWorker: true,
   theme: 'yuu',
